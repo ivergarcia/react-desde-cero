edf308d416e28d281886bd2fed0baed4ed0fa163
iver <dealth123@gmail.com>
2020-07-06 11:15:19 -0400

    primer commit del curso de react en edteam

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './styles/styles.sass';
+import Curso from './Curso';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = () => {
+  return(
+    <>
+    <div className="main-banner img-container l-section" id="main-banner">
+      <div className="ed-grid lg-grid-6">
+        <div className="lg-cols-4 lg-x-2">
+          <img className="main-banner__img" alt="banner" src="city.jpg" />
+          <div className="main-banner__data s-center">
+            <p className="t2 s-mb-0">Mi primer Proyecto en EDTeam</p>
+            <p>Realizando los cursos de edteam y viendo videos de como realizarlo</p>
+            <a href="https://www.google.com.bo" className="button">Ingresar</a>
+          </div>
+        </div>
+      </div>
     </div>
+    <div className="ed-grid m-grid-3">
+      <Curso />
+      <Curso />
+      <Curso />
+      <Curso />
+      <Curso />
+      <Curso />
+      <Curso />
+      <Curso />
+      <Curso />
+      <Curso />
+      <Curso />
+      <Curso />
+      <Curso />
+    </div>
+    </>
   );
 }
 
